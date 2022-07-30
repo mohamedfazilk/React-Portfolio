6501b22a09cca14a87f2d62b582bfbcc94a060ac
mohamedfazilk <2114921@cognizant.com>
2022-07-30 21:37:20 +0530

    importing Header component into App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
-    <div><h2>hhh</h2></div>
+    <>
+    
+    <Header/>
+    
+    </>
   )
 }
 
