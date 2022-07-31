01bfe5d5823b6b464349eae51dac18b2fa1e9170
mohamedfazilk <2114921@cognizant.com>
2022-07-31 16:50:56 +0530

    Style the CAL component buttons

--- src/components/header/CAL.jsx
+++ src/components/header/CAL.jsx
@@ -4,8 +4,8 @@ import cv from '../../assets/cv.pdf'
 const CAL = () => {
   return (
     <div className='cal'>
-        <a href={cv} download>Download CV</a>
-        <a href="#contact">Let's Talk</a>
+        <a href={cv} className='btn' download>Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
 }
