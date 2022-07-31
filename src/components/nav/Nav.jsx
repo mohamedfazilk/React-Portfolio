14b1d87f4ee753dd10a3aee5de4fd726d95a0bb5
mohamedfazilk <2114921@cognizant.com>
2022-07-31 22:22:10 +0530

    importing useState hook and  and checking the condition

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,12 +4,14 @@ import {AiOutlineHome,AiOutlineUser} from 'react-icons/ai';
 import {BsBook} from 'react-icons/bs';
 import {RiServiceLine} from 'react-icons/ri';
 import {BiMessageSquareDetail} from 'react-icons/bi';
+import {useState} from 'react';
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" className="active" ><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
+      <a href="#" className={activeNav === '#' ? 'active' : '' } ><AiOutlineHome/></a>
+      <a href="#about" ><AiOutlineUser/></a>
       <a href="#experience"><BsBook/></a>
       <a href="#services"><RiServiceLine/></a>
       <a href="#contact"><BiMessageSquareDetail/></a>
