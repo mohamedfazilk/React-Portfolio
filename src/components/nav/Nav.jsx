11df11d635d27e35da98d9a28da03e4a3d09b335
mohamedfazilk <2114921@cognizant.com>
2022-07-31 21:27:30 +0530

    add the react-icons for nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './nav.css';
+import {AiOutlineHome,AiOutlineUser} from 'react-icons/ai';
+import {BsBook} from 'react-icons/bs';
+import {RiServiceLine} from 'react-icons/ri';
+import {BiMessageSquareDetail} from 'react-icons/bi';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href=""><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
