9dea69936bbf6d804375ad6cb0f06bcba4d700a8
mohamedfazilk <2114921@cognizant.com>
2022-07-31 15:53:04 +0530

    Adding the Header component text values

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+     <div className="container header-container">
+       <h5>Hi I'm</h5>
+       <h1>MOhamed Fazil </h1>
+       <h5 className='text-light'>Full Stack Developer</h5>
+     </div>
+
+   </header>
   )
 }
 
