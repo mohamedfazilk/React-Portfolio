0653eda92bd6332564d00397882d06f51e0cad6c
mohamedfazilk <2114921@cognizant.com>
2022-08-01 12:08:32 +0530

    adding headings and contents in experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,22 @@ import './experience.css';
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+        <h5>Skills I Have</h5>
+         <h2>Experience</h2>
+         <div className="container experience__container">
+
+           <div className="front-end__experience">
+             <h3></h3>
+
+           </div>
+
+           <div className="backend__experience">
+
+           </div>
+         </div>
+
+    </section>
   )
 }
 
