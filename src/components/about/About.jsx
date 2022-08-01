4deec1a463fb6634818df45f27061dc3681f4ed4
mohamedfazilk <2114921@cognizant.com>
2022-08-01 11:15:09 +0530

    place the design for about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './about.css';
 import ME from '../../assets/fas.jpg';
 import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const about = () => {
   return (
@@ -14,17 +16,35 @@ const about = () => {
           <div className="about__me-image">
           <img src={ME} alt="ME" />
           </div>
-        
         </div>
 
         <div className="about__content">
           <div className="about__cards">
-            <div className="about__card">
+            <article className="about__card">
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
-              <small>Currently working</small>
-            </div>
+              <small>3+ years workspace </small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ workspace</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+completed</small>
+            </article>
           </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Eius tenetur ratione dicta? Beatae quaerat amet molestiae
+               sit rem illum, aliquid, placeat earum ipsa, qui itaque perferendis
+                cum numquam repellat similique.</p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>      
 
         </div>
       </div>
