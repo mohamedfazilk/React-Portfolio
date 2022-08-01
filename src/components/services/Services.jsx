91f0e3b0d304dcc3421452846ea767a35d6fa492
mohamedfazilk <2114921@cognizant.com>
2022-08-01 21:28:28 +0530

    adding service list

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,135 @@
 import React from 'react';
 import './service.css';
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => {
   return (
     <section id='services'>
       <h5>What I can Provide</h5>
-         <h2>Services</h2>
+      <h2>Services</h2>
       <div className="container service__container">
+        <article className='service'>
+
+          <div className="service__heading">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+          </ul>
+        </article>
+{/* =====================1st================== */}
+
+        <article className='service'>
+          <div className="service__heading">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </li>
+          </ul>
+        </article>
+
+        {/* =========================3============================== */}
+
+
+        <article className='service'>
+
+        <div className="service__heading">
+        <h3>Web Development</h3>
+        </div>
+        <ul className='service__list'>
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+          </li>
+
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+          </li>
+
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+          </li>
+
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+          </li>
+
+          <li>
+            <BiCheck className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur.
+              </p>
+          </li>
+
+
+
+        </ul>
+        </article>
+      
+
+
 
       </div>
     </section>
