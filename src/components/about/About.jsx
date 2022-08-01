61078052716a9304c629f9a0f5b52daf35ac15b4
mohamedfazilk <2114921@cognizant.com>
2022-08-01 10:22:24 +0530

    place the about_image in the About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/fas.jpg';
 
 const about = () => {
   return (
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
+
+      <div className="container about-container">
+        <div className="about__me">
+          <div className="about__me-image">
+          <img src={ME} alt="ME" />
+          </div>
+        
+        </div>
+
+        <div className="about__content">
+
+        </div>
+      </div>
     </section>
   )
 }
