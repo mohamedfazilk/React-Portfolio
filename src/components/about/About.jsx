4eb997f2e53c27ddecd4a6341b7f5dba0b2054e7
mohamedfazilk <2114921@cognizant.com>
2022-08-01 10:14:25 +0530

    add the h5 and h2 tag for the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,10 @@ import './about.css';
 
 const about = () => {
   return (
-    <section id='about'>about</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+    </section>
   )
 }
 
