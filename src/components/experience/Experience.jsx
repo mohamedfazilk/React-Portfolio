80b9822bc4b04fccbb285b550b5fc12a54bef78b
mohamedfazilk <2114921@cognizant.com>
2022-08-01 12:23:32 +0530

    placing the backend development details in the Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -17,11 +17,85 @@ const Experience = () => {
                  <h4>Html</h4>
                   <small className='text-light'>Experienced</small>
                </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>CSS</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Bootstrap</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Javascript</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>React </h4>
+                  <small className='text-light'>Intermediate</small>
+               </article>
+
+              
              </div>
 
            </div>
 
+           {/* ================================backend=================================== */}
+
            <div className="backend__experience">
+           <h3>Backend Development</h3>
+             <div className="experience__content">
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Java</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Spring Boot</h4>
+                  <small className='text-light'>Intermediate</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Spring Rest</h4>
+                  <small className='text-light'>Intermediate</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>MySql</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Python</h4>
+                  <small className='text-light'>Experienced</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Django</h4>
+                  <small className='text-light'>Intermediate</small>
+               </article>
+
+               <article className="experience__details">
+                 <BsPatchCheck/>
+                 <h4>Flask</h4>
+                  <small className='text-light'>Intermediate</small>
+               </article>
+
+              
+             </div>
 
            </div>
          </div>
