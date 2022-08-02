b97e8a3cf7fefcb5d9451a4329e75a17bfc3c1ef
mohamedfazilk <2114921@cognizant.com>
2022-08-02 16:26:45 +0530

    place the permalinks for all components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,21 @@ const Footer = () => {
       <footer>
         <a href="#" className='footer__logo'>FAZIL</a>
 
-        
+        <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="">facebook</a>
+          <a href="">Instagram</a>
+
+        </div>
       </footer>
     </section>
   )
