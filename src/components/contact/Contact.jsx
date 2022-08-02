1e6713f4023e90618b8e547b0794163dfedc113d
mohamedfazilk <2114921@cognizant.com>
2022-08-02 14:32:31 +0530

    add the first contact__Option

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
     <section id='contact'>
       <h5>Get   In Touch</h5>
       <h2>Contact</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            < MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>Mohamedfazil961@gmail.com</h5>
+            <a href="mailto:Mohamedfazil961@gmail.com">Send a Message</a>
+          </article>
+        </div>
+      </div>
     </section>
   )
 }
