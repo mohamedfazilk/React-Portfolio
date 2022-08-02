09ffd92668052c11dde86136971f58dc417cb7ce
mohamedfazilk <2114921@cognizant.com>
2022-08-02 11:52:55 +0530

    importing data array and iterating through map method

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,26 +4,67 @@ import IMG1 from '../../assets/avatar1.jpg'
 import IMG2 from '../../assets/avatar2.jpg';
 import IMG3 from '../../assets/avatar3.jpg';
 
+
+const data = [
+
+	{
+	id:1,
+	image:IMG1,
+	title:'dummy of every time',
+	Review:'sdfghjklfghjkl;fghjk sdfghjklfghjkl;fghjk',
+	
+	},
+
+	{
+	id:2,
+	image:IMG2,
+	title:'dummy of every time',
+	github:'sdfghjklfghjkl;fghjk',
+	
+	},
+
+{
+	id:3,
+	image:IMG3,
+	title:'dummy of every time',
+	github:'sdfghjklfghjkl;fghjk',
+	
+	},
+
+{
+	id:4,
+	image:IMG4,
+	title:'dummy of every time',
+	github:'sdfghjklfghjkl;fghjk',
+	
+	},
+
+{
+	id:5,
+	image:IMG5,
+	title:'dummy of every time',
+	github:'sdfghjklfghjkl;fghjk',
+	
+	},
+
+{
+	id:6,
+	image:IMG6,
+	title:'dummy of every time',
+	github:'sdfghjklfghjkl;fghjk',
+	
+	}
+
+]
+
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Review From Clients</h5>
       <h3>Testimonials</h3>
       <div className="container testimonials_container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={IMG1} alt="avatar-one" />
-          </div>
-
-          <h5 className='client__name'>Fazil</h5>
-          <small className='client__review'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Nostrum nesciunt officia itaque dolorum sit sequi!
-              
-          </small>
-
-
-        </article>
+       
       </div>
     </section>
   )
