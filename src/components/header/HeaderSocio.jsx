f575d375933e9e40e9f042702b12105fe0356145
mohamedfazilk <2114921@cognizant.com>
2022-08-03 15:29:32 +0530

    place the urls for the header-icons

--- src/components/header/HeaderSocio.jsx
+++ src/components/header/HeaderSocio.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs';
 const HeaderSocio = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.google.com/" target='_blank'><BsLinkedin/></a>
-        <a href="https://www.google.com/" target='_blank'><FaGithub /></a>
-        <a href="https://www.google.com/" target='_blank'><BsInstagram/></a>
+        <a href="https://www.linkedin.com/in/mohamed-fazil-k-054217196" target='_blank'><BsLinkedin/></a>
+        <a href="https://github.com/mohamedfazilk" target='_blank'><FaGithub /></a>
+        <a href="https://instagram.com/fazil_k66" target='_blank'><BsInstagram/></a>
 
     </div>
   )
